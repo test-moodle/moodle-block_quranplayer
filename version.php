5e687e1f5924ae66465ea459f190ade4eb738b36
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2025-01-11 19:21:27 +0400

    mp3 files

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_audioplayer';
+$plugin->version = 2025011000; // YYYYMMDDXX
+$plugin->requires = 2020110900; // Minimum Moodle version
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
